4234de4872152e12ef4c93ba932864256dfc2f9b
Sahir Mody <sahir.mody@gmail.com>
2019-08-11 11:48:38 -0400

    Remove header

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var i = 0;
 
 // array of strings to store the content on the site
-var txt = ['Hey! I\'m Sahir and I\'m a sophomore at the ',
+var txt = ['Hey! I\'m Sahir Mody and I\'m a sophomore at the ',
                'University of Maryland.',
                'I\'m currently studying ',
                'Computer Science',
